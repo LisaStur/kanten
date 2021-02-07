72d81456ece707418ee46995c1571d207dfb76ec
Lisa Sturegard <lisasturegard@hotmail.com>
2021-02-07 18:53:12 +0100

    Added map to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,20 +13,25 @@ export const Contact = () => {
           <TimeTableLink href="http://www.kostermarin.se/">Koster Marins Tidtabell</TimeTableLink>
           <br /> Eller använd Västtrafiks reseplanerare med "från" Strömstad norra hamnen, Strömstad och "till" Koster Västra Bryggan, Strömstad här: 
           <TimeTableLink href="https://www.vasttrafik.se/">Västtrafiks reseplanerare</TimeTableLink>
-          <br />Om du kommer med egen båt finns det en gästhamn bredvid Strandkanten att lägga till vid.
         </ContactText>
+        <ContactText>
+          Om du kommer med egen båt finns det en gästhamn bredvid Strandkanten att lägga till vid.
+          </ContactText>
       </ContactTextSection>
+      <MapBox>
+        <iframe title="Strandkanten map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d515.3008139394348!2d11.011601829256326!3d58.89448019884429!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTjCsDUzJzQwLjEiTiAxMcKwMDAnNDMuNyJF!5e0!3m2!1ssv!2sse!4v1612715756096!5m2!1ssv!2sse" width="300" height="200" frameborder="0" style={{border:0}} allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        </MapBox>
     </ContactSection>
   )
 }
-
 const ContactSection = styled.section`
   display: flex;
   flex-direction: column;
   margin: 12px;
+  padding-bottom: 8px;
   background-color: rgba(100,100,130, 0.8);
 
-  @media (min-width: 668px) {
+  @media (min-width: 1024px) {
     display: flex;
     flex-direction: row;
   }  
@@ -58,4 +63,13 @@ const TimeTableLink = styled.a`
     background-color: rgba(80,80,100);
     transition: 0.4s;
   }
+`
+const MapBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-item: center;
+  
+  @media (min-width: 668px) {
+    padding: 2%;
+  }  
 `
